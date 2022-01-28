87b8e0c20790e93360eb9b72480ee1f594fc3683
prxtik69 <pratikdeshpande007pvd@gmail.com>
2022-01-28 14:08:25 +0530

    added convert to lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 
+
 export default function TextForm(props) {
-  const [text, useText] = useState("Enter Your Text");
-  const Changetext = () => {
-    // console.log("Change Text");
-    let Newtext = text.toUpperCase();
-    useText(Newtext);
+  const [text, useText] = useState("");
+  const ChangeToUpperCase = () => {
+    let UpperCaseText = text.toUpperCase();
+    useText(UpperCaseText);
   };
+
+  const ChangeToLowerCase = () => {
+    let LowerCaseText = text.toLowerCase();
+    useText(LowerCaseText);
+  }
 
   const ChangedEvent = (event) => {
     useText(event.target.value);
   };
   return (
-    <div>
-      <h1>{props.heading}</h1>
+    <>
+    <div className="container">
+      <h2>{props.heading}</h2>
       <div className="mb-3">
         <textarea
           className="form-control"
@@ -23,9 +29,22 @@ export default function TextForm(props) {
           onChange={ChangedEvent}
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={Changetext}>
+      <button className="btn btn-outline-primary" onClick={ChangeToUpperCase}>
         Convert to Uppercase
       </button>
+      <button className="btn btn-outline-danger" onClick={ChangeToLowerCase}>
+        Convert to Lowercase
+      </button>
     </div>
+
+    <div className="container my-5">
+      <h1>Your Text Summary</h1>
+     
+      <p><b>Your Text Has :  <code>{text.split(" ").length - 1} Words and {text.length} Characters</code></b></p>
+      <p>Your Text Took {0.008 * text.split(" ").length} minutes to read.</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
