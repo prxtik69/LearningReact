297559bf420111955b69167bafcdfe13e26a12fc
meisolated <isolatedbot@gmail.com>
2022-01-29 14:44:01 +0530

    Fixed Bug

--- src/App.js
+++ src/App.js
@@ -1,98 +1,86 @@
-import React, { useState } from 'react';
-import "./App.css";
-import Alert from "./components/Alert";
-import About from "./components/About";
-import TextForm from "./components/TextForm";
-import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useState, Fragment } from "react"
+import "./App.css"
+import Alert from "./components/Alert"
+import About from "./components/About"
+import TextForm from "./components/TextForm"
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
+ const [mode, setMode] = useState("light")
+ const [alert, setAlert] = useState(null)
 
-  const [mode, setMode] = useState("light");
-  const [alert, setAlert] = useState(null);
+ const showAlert = (message, type) => {
+  setAlert({
+   msg: message,
+   type: type,
+  })
 
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type
-    })
+  setTimeout(() => {
+   setAlert(null)
+  }, 1500)
+ }
+ const toggleMode = () => {
+  if (mode === "light") {
+   setMode("dark")
+   document.title = "Pratik's App | Dark Mode"
+   showAlert("Dark Mode Enabled", "success")
 
-    setTimeout(() => {
-      setAlert(null)
-    }, 1500);
+   document.body.style.backgroundColor = "#3d3d3b"
+   document.body.style.color = "white"
+  } else {
+   setMode("light")
+   document.title = "Pratik's App | Light Mode"
+   showAlert("Dark Mode Disabled", "success")
+
+   document.body.style.backgroundColor = "#f5f5f5"
+   document.body.style.color = "black"
   }
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.title = "Pratik's App | Dark Mode";
-      showAlert("Dark Mode Enabled", "success");
+ }
 
-      document.body.style.backgroundColor = "#3d3d3b";
-      document.body.style.color = "white";
-    }
-    else {
-      setMode("light");
-      document.title = "Pratik's App | Light Mode";
-      showAlert("Dark Mode Disabled", "success");
+ const ChangeColorRed = () => {
+  document.title = "Pratik's App | Red Mode"
+  document.body.style.backgroundColor = "#5c1b11"
+  document.body.style.color = "white"
+  document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white"
+  document.getElementById("exampleFormControlTextarea1").style.color = "black"
+  showAlert("Background Color Changed to Red", "success")
+ }
+ const ChangeColorGreen = () => {
+  document.title = "Pratik's App | Green Mode"
+  document.body.style.backgroundColor = "#03691e"
+  document.body.style.color = "white"
+  document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white"
+  document.getElementById("exampleFormControlTextarea1").style.color = "black"
+  showAlert("Background Color Changed to Green", "success")
+ }
 
-      document.body.style.backgroundColor = "#f5f5f5";
-      document.body.style.color = "black";
-    }
-  }
-
-  const ChangeColorRed = () => {
-    document.title = "Pratik's App | Red Mode";
-    document.body.style.backgroundColor = "#5c1b11";
-    document.body.style.color = "white";
-    document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white";
-    document.getElementById("exampleFormControlTextarea1").style.color = "black";
-    showAlert("Background Color Changed to Red", "success");
-  }
-  const ChangeColorGreen = () => {
-    document.title = "Pratik's App | Green Mode";
-    document.body.style.backgroundColor = "#03691e";
-    document.body.style.color = "white";
-    document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white";
-    document.getElementById("exampleFormControlTextarea1").style.color = "black";
-    showAlert("Background Color Changed to Green", "success");
-  }
-
-  const ChangeColorYellow = () => {
-    document.title = "Pratik's App | Yellow Mode";
-    document.body.style.backgroundColor = "#898f15";
-    document.body.style.color = "white";
-    document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white";
-    document.getElementById("exampleFormControlTextarea1").style.color = "black";
-    showAlert("Background Color Changed to Yellow", "success");
-  }
-  return (
-    <>
-
-
-      <Router>
-        <Navbar title="MyApp" aboutName="About My Page" mode={mode} toggleMode={toggleMode} ChangeColorRed={ChangeColorRed} ChangeColorGreen={ChangeColorGreen} ChangeColorYellow={ChangeColorYellow} />
-        <Alert alertMessage={alert} />
-        <div className="container my-3">
-
-          <Routes>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <TextForm heading="Enter Your Text to Analyze" mode={mode} showAlert={showAlert} />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-
-
-
-    </>
-  );
+ const ChangeColorYellow = () => {
+  document.title = "Pratik's App | Yellow Mode"
+  document.body.style.backgroundColor = "#898f15"
+  document.body.style.color = "white"
+  document.getElementById("exampleFormControlTextarea1").style.backgroundColor = "white"
+  document.getElementById("exampleFormControlTextarea1").style.color = "black"
+  showAlert("Background Color Changed to Yellow", "success")
+ }
+ return (
+  <>
+   <Router>
+    <Fragment>
+     <Navbar title="MyApp" aboutName="About My Page" mode={mode} toggleMode={toggleMode} ChangeColorRed={ChangeColorRed} ChangeColorGreen={ChangeColorGreen} ChangeColorYellow={ChangeColorYellow} />
+     <Alert alertMessage={alert} />
+     <div className="container my-3">
+      <Routes>
+       <Route path="/about" element={<About />}></Route>
+       <Route path="/" element={<TextForm heading="Enter Your Text to Analyze" mode={mode} showAlert={showAlert} />}>
+        {/* <TextForm /> */}
+       </Route>
+      </Routes>
+     </div>
+    </Fragment>
+   </Router>
+  </>
+ )
 }
 
-export default App;
+export default App
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,77 +1,72 @@
-import React, { useState } from "react";
-
+import React, { useState } from "react"
 
 export default function TextForm(props) {
+ const style = {
+  backgroundColor: props.mode === "light" ? "#3d3d3b" : "white",
+  color: props.mode === "light" ? "white" : "black",
+ }
+ const [text, useText] = useState("")
+ const ChangeToUpperCase = () => {
+  let UpperCaseText = text.toUpperCase()
+  useText(UpperCaseText)
+  props.showAlert(`Text is now in UPPER CASE`, "success")
+ }
 
-  const style = {
-    backgroundColor: props.mode === "light" ? "#3d3d3b" : "white",
-    color: props.mode === "light" ? "white" : "black"
-  }
-  const [text, useText] = useState("");
-  const ChangeToUpperCase = () => {
-    let UpperCaseText = text.toUpperCase();
-    useText(UpperCaseText);
-    props.showAlert(`Text is now in UPPER CASE`, "success");
-  };
+ const ChangeToLowerCase = () => {
+  let LowerCaseText = text.toLowerCase()
+  useText(LowerCaseText)
+  props.showAlert(`Text is now in LOWER CASE`, "success")
+ }
 
-  const ChangeToLowerCase = () => {
-    let LowerCaseText = text.toLowerCase();
-    useText(LowerCaseText);
-    props.showAlert(`Text is now in LOWER CASE`, "success");
-  }
+ const ClearText = () => {
+  useText("")
+  props.showAlert(`Text is cleared now`, "success")
+ }
 
-  const ClearText = () => {
-    useText("");
-    props.showAlert(`Text is cleared now`, "success");
-  }
+ const ChangedEvent = (event) => {
+  useText(event.target.value)
+ }
 
-  
+ function wordcounter(text) {
+  let wordNumber = text.split(" ").filter(function (n) {
+   return n !== ""
+  }).length
+  return wordNumber
+ }
 
-  const ChangedEvent = (event) => {
-    useText(event.target.value);
-  };
-
-  function wordcounter(text) {
-    let wordNumber = text.split(' ').filter(
-        function (n) { return n !== '' }
-    ).length;
-    return wordNumber;
-}
-
-  return (
-    <>
-    <div className="container">
-      <h2>{props.heading}</h2>
-      <div className="mb-3">
-        <textarea
-        style={style}
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          value={text}
-          onChange={ChangedEvent}
-        ></textarea>
-      </div>
-      <button className="btn btn-outline-primary mx-2" onClick={ChangeToUpperCase}>
-        Convert to Uppercase
-      </button>
-      <button className="btn btn-outline-danger mx-2" onClick={ChangeToLowerCase}>
-        Convert to Lowercase
-      </button>
-      <button className="btn btn-outline-warning mx-2" onClick={ClearText}>
-        Clear Text
-      </button>
-      
+ return (
+  <>
+   <div className="container">
+    <h2>{props.heading}</h2>
+    <div className="mb-3">
+     <textarea style={style} className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={ChangedEvent}></textarea>
     </div>
+    <button className="btn btn-outline-primary mx-2" onClick={ChangeToUpperCase}>
+     Convert to Uppercase
+    </button>
+    <button className="btn btn-outline-danger mx-2" onClick={ChangeToLowerCase}>
+     Convert to Lowercase
+    </button>
+    <button className="btn btn-outline-warning mx-2" onClick={ClearText}>
+     Clear Text
+    </button>
+   </div>
 
-    <div className="container my-5">
-      <h1>Your Text Summary</h1>
-     
-      <p><b>Your Text Has :  <code>{wordcounter(text)} Words and {text.length} Characters</code></b></p>
-      <p>Your Text Took {0.008 * wordcounter(text)} minutes to read.</p>
-      <h2>Preview</h2>
-      <p>{text.length > 0 ? text : "Enter Something in the Text Box above to preview it here."}</p>
-    </div>
-    </>
-  );
+   <div className="container my-5">
+    <h1>Your Text Summary</h1>
+
+    <p>
+     <b>
+      Your Text Has :{" "}
+      <code>
+       {wordcounter(text)} Words and {text.length} Characters
+      </code>
+     </b>
+    </p>
+    <p>Your Text Took {0.008 * wordcounter(text)} minutes to read.</p>
+    <h2>Preview</h2>
+    <p>{text.length > 0 ? text : "Enter Something in the Text Box above to preview it here."}</p>
+   </div>
+  </>
+ )
 }
